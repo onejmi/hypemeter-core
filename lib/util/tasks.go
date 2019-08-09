a8097dd512887b693376fdbc7238bde1e6a39959
scarger <supremeterminal@gmail.com>
2019-08-08 23:52:21 -0400

    debug addition

--- lib/util/tasks.go
+++ lib/util/tasks.go
@@ -9,21 +9,19 @@ import (
 )
 
 func SetupCronJobs() {
-	gocron.Every(3).Minutes().Do(invalidateExpiredSessions)
+	gocron.Every(2).Minutes().Do(invalidateExpiredSessions)
 	gocron.Start()
 }
 
 func invalidateExpiredSessions() {
-	fmt.Println("logging")
 	cursor := data.List("sessions", bson.D{{}})
-	fmt.Println("double logz")
 
 	for {
-		fmt.Println("ok somewhere")
 		var session data.Session
 		attemptError := cursor.Decode(&session)
 		fmt.Println("wait for it...")
 		if attemptError != nil {
+			fmt.Println(attemptError.Error())
 			break
 		}
 		fmt.Println("wait what")
